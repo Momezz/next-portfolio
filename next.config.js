0810417afe7885b96c7227409001a652dfe0841c
Momezz <julgomez14@gmail.com>
2023-07-03 00:30:45 -0500

    create presentation component

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        domains: ['tse4.explicit.bing.net']
+    }
+}
 
 module.exports = nextConfig
