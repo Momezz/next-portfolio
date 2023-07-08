8caac1946b67f9d3b9fd0284d1c1913bd8f2ea2e
Momezz <julgomez14@gmail.com>
2023-07-08 08:01:34 -0500

    create card for strengths and hobbies

--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -1,6 +1,13 @@
+import Card from '../../components/Card';
+import TrainingCard from '../../components/TrainingCard';
+
 const Portfolio = () => {
     return (
-        <h1>PORTFOLIO</h1>
+        <>
+            <h1>PORTFOLIO</h1>
+            <Card />
+            <TrainingCard />
+        </>
     )
 }
 
---
+++ src/components/TrainingCard.jsx
@@ -0,0 +1,7 @@
+const TrainingCard = () => {
+    return(
+        <h1>card</h1>
+    )
+}
+
+export default TrainingCard;
