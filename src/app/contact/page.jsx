55166e64531e3f2abe0e11dca0a081cb9bde8167
Momezz <julgomez14@gmail.com>
2023-08-07 17:16:01 -0500

    install editorconfig

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -2,11 +2,11 @@ import ContactForm from '../../components/ContactForm';
 import styles from './cotact.module.css';
 
 const Contact = () => {
-    return (
-        <section className={styles.contac__container}>
-            <ContactForm />
-        </section>
-    )
+  return (
+    <section className={styles.contac__container}>
+      <ContactForm />
+    </section>
+  )
 }
 
 export default Contact;
