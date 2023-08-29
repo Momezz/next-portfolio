9db42b71940444a922afb39d9def54be47ce329e
Momezz <julgomez14@gmail.com>
2023-08-28 22:21:54 -0500

    I improved view of the skills

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,19 +1,39 @@
-import styles from "../components/card.module.css";
-import PropTypes from "prop-types";
-import Image from "next/image";
+import styles from '../components/card.module.css';
+import PropTypes from 'prop-types';
+import Image from 'next/image';
 
 const Card = ({ element }) => {
   return (
     <article className={styles.card__container}>
-      <Image
-        className={styles.card__image}
-        src={element.image}
-        alt={element.ability}
-        width={110}
-        height={120}
-        priority
-      />
       <h2 className={styles.card__title}>{element.ability}</h2>
+      <div className={styles.card__level}>
+        <span>★</span>
+        <span>★</span>
+        <span>★</span>
+        <span>★</span>
+        <span>★</span>
+      </div>
+      <div className={styles.card__image_cont}>
+        <Image
+          className={styles.card__image}
+          src={element.image}
+          alt={element.ability}
+          width={110}
+          height={120}
+          priority
+        />
+      </div>
+      <div className={styles.card__skill_type_cont}>
+        <h3 className={styles.card__skill_type_title}>[Habilidad/Tecnica]</h3>
+        <p className={styles.card__skill_type_paragraph}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas,
+          enim eveniet.
+        </p>
+        <hr className={styles.card__skill_hr} />
+      </div>
+      <div className={styles.card__skill_copyright}>
+        <span>©2023 MOMEZZ.</span>
+      </div>
     </article>
   );
 };
