91ebc6bdc4e4e23fa930f504aeb1e366011fc2e8
Momezz <julgomez14gmail.com>
2024-01-09 23:00:47 -0500

    link message form with nodemailer on server

--- src/services/messages.js
+++ src/services/messages.js
@@ -1,36 +1,23 @@
-/* eslint-disable no-unused-vars */
-import { createAsyncThunk } from '@reduxjs/toolkit';
 
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
-export const createMessage = createAsyncThunk(
-  'message/createMessage',
-  async (message) => {
-    const options = {
-      method: 'POST',
+export const sendMail = async (values) => {
+  let message = false;
+  try {
+    const response = await fetch(`${BASE_URL}send-mail`, {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify(message),
-    };
-
-    const res = await fetch(`${BASE_URL}api/messages`, options);
-    const result = await res.json();
-    return result;
-  },
-);
-export const getMessageById = createAsyncThunk(
-  'message/getMessage',
-  async (data) => {
-    const options = {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    };
-
-    const res = await fetch(`${BASE_URL}api/messages/${data}`, options);
-    const result = await res.json();
-    return result;
-  },
-);
+      body: JSON.stringify(values),
+    });
+    if (response.ok) {
+      message = true;
+    } else {
+      throw new Error(`Network error: ${response.status}`);
+    }
+  } catch (error) {
+    throw new Error(error);
+  }
+  return message;
+}
